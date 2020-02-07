8a072f44532ca5495b7f8f83fd234885e5153387
Guilhem VORS <gvors@eden.loc>
2020-02-07 14:00:29 +0100

    First commit

---
+++ New Unity Project/Assets/ButtonClickHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClickHandler : MonoBehaviour
+{
+	public ButtonContainer buttonContainer;
+	public Image image;
+
+	public void SetColored(Color color)
+	{
+		image.color = color;
+	}
+
+	public void OnClick()
+	{
+		if (image.color != Color.white)
+		{
+			buttonContainer.IncreaseScore();
+			SetColored(Color.white);
+		}
+	}
+}
